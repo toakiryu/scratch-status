9f57749c21e09b03aa8c27b3b758764f86a0ed2c
Fun117 <toakiryu@gmail.com>
2024-12-31 23:24:02 +0900

    Update page.tsx

--- website/src/app/[locale]/page.tsx
+++ website/src/app/[locale]/page.tsx
@@ -1,12 +1,9 @@
 import Footer from "@/components/footer";
 import { GiscusContent } from "@/components/giscus";
 import { StatusMonitorElement } from "@/components/monitor";
-import { getLocale } from "next-intl/server";
 import { Suspense } from "react";
 
 export default async function Home() {
-  const lang = await getLocale();
-
   return (
     <div className="flex flex-col justify-center items-center w-full h-full min-h-dvh">
       <Suspense>
